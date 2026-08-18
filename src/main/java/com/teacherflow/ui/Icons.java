c7487768586d787e82dc05e1cb333531c545ceff
alois duvoye <alois.duvoye@gmail.com>
2026-08-18 20:53:37 +0200

    Retour aux fenêtres pop-up pour l'emploi du temps

--- src/main/java/com/teacherflow/ui/Icons.java
+++ src/main/java/com/teacherflow/ui/Icons.java
@@ -12,18 +12,12 @@ final class Icons {
 
     private static final String CHEMIN_POUBELLE =
             "M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z";
-    private static final String CHEMIN_FERMER =
-            "M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z";
 
     private Icons() {
     }
 
     static Node poubelle() {
         return icone(CHEMIN_POUBELLE, "icone-poubelle");
-    }
-
-    static Node fermer() {
-        return icone(CHEMIN_FERMER, "icone-fermer");
     }
 
     private static Node icone(String chemin, String classeStyle) {
