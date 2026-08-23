ddc1d6e21415886e118ceb6b8ba52c8507a6a870
alois duvoye <alois.duvoye@gmail.com>
2026-08-23 20:48:08 +0200

    Ajoute AtlantaFX (dépendance + module JPMS)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.teacherflow.app {
     requires javafx.controls;
     requires javafx.fxml;
+    requires atlantafx.base;
     requires com.fasterxml.jackson.databind;
     requires com.fasterxml.jackson.datatype.jsr310;
 
